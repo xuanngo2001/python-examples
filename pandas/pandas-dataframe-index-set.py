5b5a093b6df8aacee5802c04134f737f580f3e77
limelime <limelime@users.noreply.github.com>
2020-03-06 15:19:47 -0500

    Add index py.

---
+++ pandas/pandas-dataframe-index-set.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from io import StringIO
+import pandas as pd
+ 
+data="""Date,Stock,Open,High,Low,Close,Volume
+2016-09-29,KESM,7.92,7.98,7.92,7.97,149400
+2016-09-30,KESM,7.96,7.97,7.84,7.9,29900
+2016-10-04,KESM,7.8,7.94,7.8,7.93,99900
+2016-10-05,KESM,7.93,7.95,7.89,7.93,77500
+2016-10-06,KESM,7.93,7.93,7.89,7.92,130600
+2016-10-07,KESM,7.91,7.94,7.91,7.92,103000"""
+
+# Load data and set Date as index column.
+df = pd.read_csv(StringIO(data), index_col='Date', parse_dates=True)
+
+# Reset index to preserve 'Date' data.
+df.reset_index(inplace = True)
+
+# Change DataFrame index to 'Stock'.
+df.set_index(['Stock'], drop = True, append = False, inplace = True)
+
+# Print outcome.
+print(df)
