91186190a7cab194cf88c95c6651c84c74706e83
Xuan Ngo <xuan.ngo@example.com>
2020-10-18 11:48:42 -0400

    u

---
+++ net/net-listen.py
@@ -0,0 +1,19 @@
+# Credit: https://github.com/KalleHallden/reddit_automations/blob/master/net-listen.py
+import sys
+import subprocess 
+import os
+from decouple import config
+
+IP_NETWORK = config('IP_NETWORK')
+IP_DEVICE = config('IP_DEVICE')
+
+proc = subprocess.Popen(["ping", IP_NETWORK],stdout=subprocess.PIPE)
+while True:
+  line = proc.stdout.readline()
+  if not line:
+    break
+  #the real code does filtering here
+  connected_ip = line.decode('utf-8').split()[3]
+
+  if connected_ip == IP_DEVICE:
+      subprocess.Popen(["say", "Linnea just connected to the network"])
