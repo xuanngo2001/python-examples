fe0afacfde22aef3223f4a5e20b3548b0ebf49a8
Xuan Ngo <xuan.ngo@example.com>
2021-02-25 10:59:33 -0500

    u

---
+++ string/find_replace.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+# Find and replace: No regular expression
+s = 'Find and replace'
+r = s.replace('Find', 'Search')
+print(r) # Output: Search and replace
+
+# Find and replace within list.
+words = ['ad', 'add']
+r = [w.replace('ad', 'XX') for w in words]
+print(r) # Output: ['XX', 'XXd']
+
+# Find and replace whole words with list.
+import re
+words = ['ad', 'add']
+# r for raw string(literal), \b for boundary.
+r = [re.sub(r'\bad\b', 'XX', w) for w in words]
+print(r) # Output: ['XX', 'add']
+
+# ---------------------- Use replacements dictionary.
+# https://stackoverflow.com/a/17730939
+import re
+
+replacements = {'ad':'XX', 
+                'add':'SSSS'}
+
+words = ['ad', 'add']
+def replace(match):
+    return replacements[match.group(0)]
+
+r =  [re.sub('|'.join(r'\b%s\b' % re.escape(s) for s in replacements)
+            , replace
+            , w)
+            for w in words]
+print(r) # Output: ['XX', 'SSSS']
